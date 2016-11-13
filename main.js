0a42e0f25b56054461eab59f708db385b3796ca6
Andreas Ostlund <andreasostlund@Andreass-iMac.local>
2016-11-13 18:41:10 +0100

    addText now keeps the children

--- main.js
+++ main.js
@@ -3,51 +3,59 @@ var addP = $('<li>Add p</li>').attr('id', 'add_p')
 var addText = $('<li>Add text</li>').attr('id', 'add_text')
 var elemId = 0
 
-$(function() {
-   $('#container').on('click', '#start', function(event) {
-     if (event.target.id == $(this).attr('id')) { // only trigger this if we really pressed the #start div
-        showMenu($(this))
-     } 
-   })
-   $('#container').on('click', 'li', function() {
-       addSomething($(this))
-   })
+$(function () {
+  $('#container').on('click', '#start', function (event) {
+    if (event.target.id == $(this).attr('id')) { // only trigger this if we really pressed the #start div
+      showMenu($(this))
+    }
+  })
+  $('#container').on('click', 'li', function () {
+    addSomething($(this))
+  })
 })
 
-var showMenu = function(element) {
-    element.append($('<ul></ul>').attr('id', 'add_ul'))
-    $('#add_ul').append(addDiv, addText, addP)  
+var showMenu = function (element) {
+  element.append($('<ul></ul>').attr('id', 'add_ul'))
+  $('#add_ul').append(addDiv, addText, addP)
 }
 
-var addSomething = function(element) {
-    switch(element.attr('id')) {
-        case 'add_div':
-            addElement(element.parent().parent(), '<div></div>')
-            break
-        case 'add_p':
-            addElement(element.parent().parent(), '<p></p>')
-            break
-        case 'add_text':
-            addTxt(element.parent().parent(), 'text')
-            break
-    }
+var addSomething = function (element) {
+  switch (element.attr('id')) {
+    case 'add_div':
+      addElement(element.parent().parent(), '<div></div>')
+      removeMenu()
+      break
+    case 'add_p':
+      addElement(element.parent().parent(), '<p></p>')
+      removeMenu()
+      break
+    case 'add_text':
+      addTxt(element.parent().parent(), 'text')
+      removeMenu()
+      break
+  }
 }
 
-var addElement = function(element, newElem) {
-    var newId = 'id' + elemId
-    element.append($(newElem)
-        .text('new element')
-        .attr('id', newId)
-        .on('click', function() { 
-            if (event.target.id == $(this).attr('id')) {
-                showMenu($(this)) 
-            }
-        }))
-    elemId++
-    $('#add_ul').remove()
+var addElement = function (element, newElem) {
+  var newId = 'id' + elemId
+  element.append($(newElem)
+    .text('new element')
+    .attr('id', newId)
+    .on('click', function () {
+      if (event.target.id == $(this).attr('id')) {
+        showMenu($(this))
+      }
+    }))
+  elemId++
 }
 
-var addTxt = function(element, text) {
-    element.text(text)
-    $('#add_ul').remove()
+var addTxt = function (element, text) {
+  var withDataAndEvents = true;
+  var deepWithDataAndEvents = true;
+  var temp = element.children().clone(withDataAndEvents, deepWithDataAndEvents) //uses clone(true, true) to make the copy contain the eventhandlers of the children
+  element.text(text).append(temp)
+}
+
+var removeMenu = function() {
+  $('#add_ul').remove()
 }
